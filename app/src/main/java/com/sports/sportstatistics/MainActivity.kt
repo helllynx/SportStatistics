ce8d1b708d78be8829f63f9e8143e824d5977fba
Boris <boristul97@gmail.com>
2020-02-22 18:25:32 +0300

    Add navigation

--- app/src/main/java/com/sports/sportstatistics/MainActivity.kt
+++ app/src/main/java/com/sports/sportstatistics/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.sports.sportstatistics
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        findViewById<Button>(R.id.am_button).setOnClickListener {
-            startActivity(
-                Intent(this, SecondActivity::class.java)
-                    .putExtra("Key", "Sport Statistics")
-            )
+        val navigation = findViewById<BottomNavigationView>(R.id.am_navigation)
+        val navController = findNavController(R.id.am_nav_host_fragment)
+
+        navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home -> {
+//                    navController.navigate(R.id.action_global_home)
+                    true
+                }
+                R.id.menu_info -> {
+//                    navController.navigate(R.id.action_global_transactions)
+                    true
+                }
+                R.id.menu_settings -> {
+//                    navController.navigate(R.id.action_global_settings)
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
