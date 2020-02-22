169b2b00f70397dfdff118ee9c9753b1584cae4a
Boris <boristul97@gmail.com>
2020-02-22 22:19:23 +0300

    Add local action in navigation. Add fragments to SecondActivity

--- app/src/main/java/com/sports/sportstatistics/main/MainActivity.kt
+++ app/src/main/java/com/sports/sportstatistics/main/MainActivity.kt
@@ -1,19 +1,23 @@
-package com.sports.sportstatistics
+package com.sports.sportstatistics.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sports.sportstatistics.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val navigation = findViewById<BottomNavigationView>(R.id.am_navigation)
-        val navController = findNavController(R.id.am_nav_host_fragment)
+        val navController = findNavController(R.id.am_nav_host_fragment).apply {
+            addOnDestinationChangedListener { _, destination, _ ->
+                title = destination.label
+            }
+        }
 
-        navigation.setOnNavigationItemSelectedListener { item ->
+        findViewById<BottomNavigationView>(R.id.am_navigation).setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.menu_home -> {
                     navController.navigate(R.id.action_global_homeFragment)
